f340e92562d759b518812057de77530bb41c6bda
nsktdeluxe <tomominishikata@gmail.com>
2021-09-01 00:06:15 +0900

    bomtsuki?

--- performances.php
+++ performances.php
@@ -1,116 +1,119 @@
 <!DOCTYPE HTML>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="favicon.ico">
-        <link href="css/index.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <?php include __DIR__.'/header.php'; ?>
-        <div class="content">
-            <div class="container">
-                <!-- 出演歴 -->
-                <div class="row performances">
-                    <div class="col-8">
-                        <h2>出演予定</h2>
-                        <p class="">以下のオーケストラの演奏会などに出演予定です。</p>
-                        <p class="indented-text">
-                            <span>04.17</span>
-                            <span>シンフォニックアンサンブルブーケ 第２回定期演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>05.05</span>
-                            <span>アーベント・フィルハーモニカー 第24回定期演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>07.17</span>
-                            <span>シンフォニックアンサンブルブーケ 第３回定期演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>07.23</span>
-                            <span>オオルリ弦楽アンサンブル 第４回演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                    </div>
-                    <div class="col-4">
-                        <img src="img/music-2509582_1920.jpg" alt="" class="img" />
-                    </div>
-                    <div class="col-12">
-                        <h2>過去の演奏会</h2>
-                        <h4>2021</h4>
-                        <p class="indented-text">
-                            <span>02.21</span>
-                            <span>だいたい平成音楽会「オーケストラ生配信大作戦！！」</span>
-                            <span>1stVn</span>
-                        </p>
-                        <h4>2020</h4>
-                        <p class="indented-text">
-                            <span>11.22</span>
-                            <span>ISP -Innovation in Sounds Philharmonic- 第６回演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>11.07</span>
-                            <span>ボヘミアン・フィルハーモニック 第４回定期演奏会</span>
-                            <span>1stVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>09.06</span>
-                            <span>アーベント・フィルハーモニカー 第23回定期演奏会</span>
-                            <span>1stVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>05.03</span>
-                            <span><s>Orchestra Walnuts 特別演奏会</s>(中止)</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>02.29</span>
-                            <span>シェーナークライスフィルハーモニー管弦楽団　第１回定期演奏会</span>
-                            <span>2ndVn -主席奏者</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>02.09</span>
-                            <span>毘沙門天管弦楽団 第57回演奏会</span>
-                            <span>1stVn -賛助</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>01.25</span>
-                            <span>シンフォニックアンサンブルブーケ 創立記念特別演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                        <h4>2019</h4>
-                        <p class="indented-text">
-                            <span>11.30</span>
-                            <span>ISP -Innovation in Sounds Philharmonic- 第５回演奏会</span>
-                            <span>2ndVn</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>02.23</span>
-                            <span>大妻女子大学管弦楽団</span>
-                            <span>1stVn -賛助</span>
-                        </p>
-                        <p class="indented-text">
-                            <span>01.19</span>
-                            <span><a href="https://twitter.com/search?q=%23%E3%83%81%E3%83%A3%E3%82%A4%E3%82%B3456&src=typed_query">#チャイコ456</a></span>
-                            <span>1stVn</span>
-                        </p>
-                        <p class="text-right archives_link">
-                            <a href="https://twpf.jp/nsktdeluxe/" target=”_blank”>>> もっと過去の演奏会はこちらから</a>
-                        </p>
-                    </div>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href="css/index.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include __DIR__ . '/header.php'; ?>
+    <div class="content">
+        <div class="container">
+            <!-- 出演歴 -->
+            <div class="row performances">
+                <div class="col-8">
+                    <h2>出演予定</h2>
+                    <p class="">以下のオーケストラの演奏会などに出演予定です。</p>
+                    <p class="indented-text">
+                        <span>09.05</span>
+                        <span>アーベント・フィルハーモニカー 第25回定期演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
                 </div>
-                <!-- ページトップへボタン -->
-                <!--<div id="page_top">
+                <div class="col-4">
+                    <img src="img/music-2509582_1920.jpg" alt="" class="img" />
+                </div>
+                <div class="col-12">
+                    <h2>過去の演奏会</h2>
+                    <h4>2021</h4>
+                    <p class="indented-text">
+                        <span>02.21</span>
+                        <span>だいたい平成音楽会「オーケストラ生配信大作戦！！」</span>
+                        <span>1stVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>04.17</span>
+                        <span>シンフォニックアンサンブルブーケ 第２回定期演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>07.17</span>
+                        <span>シンフォニックアンサンブルブーケ 第３回定期演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>07.23</span>
+                        <span>オオルリ弦楽アンサンブル 第４回演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
+                    <h4>2020</h4>
+                    <p class="indented-text">
+                        <span>01.25</span>
+                        <span>シンフォニックアンサンブルブーケ 創立記念特別演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>02.09</span>
+                        <span>毘沙門天管弦楽団 第57回演奏会</span>
+                        <span>1stVn -賛助</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>02.29</span>
+                        <span>シェーナークライスフィルハーモニー管弦楽団　第１回定期演奏会</span>
+                        <span>2ndVn -主席奏者</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>05.03</span>
+                        <span><s>Orchestra Walnuts 特別演奏会</s>(中止)</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>09.06</span>
+                        <span>アーベント・フィルハーモニカー 第23回定期演奏会</span>
+                        <span>1stVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>11.07</span>
+                        <span>ボヘミアン・フィルハーモニック 第４回定期演奏会</span>
+                        <span>1stVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>11.22</span>
+                        <span>ISP -Innovation in Sounds Philharmonic- 第６回演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
+                    <h4>2019</h4>
+                    <p class="indented-text">
+                        <span>01.19</span>
+                        <span><a href="https://twitter.com/search?q=%23%E3%83%81%E3%83%A3%E3%82%A4%E3%82%B3456&src=typed_query">#チャイコ456</a></span>
+                        <span>1stVn</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>02.23</span>
+                        <span>大妻女子大学管弦楽団</span>
+                        <span>1stVn -賛助</span>
+                    </p>
+                    <p class="indented-text">
+                        <span>11.30</span>
+                        <span>ISP -Innovation in Sounds Philharmonic- 第５回演奏会</span>
+                        <span>2ndVn</span>
+                    </p>
+                    <p class="text-right archives_link">
+                        <a href="https://twpf.jp/nsktdeluxe/" target=”_blank”>>> もっと過去の演奏会はこちらから</a>
+                    </p>
+                </div>
+            </div>
+            <!-- ページトップへボタン -->
+            <!--<div id="page_top">
                     <a href="#"></a>
                 </div> -->
-            </div>
         </div>
-        <?php include __DIR__.'/footer.php'; ?>
-    </body>
+    </div>
+    <?php include __DIR__ . '/footer.php'; ?>
+</body>
+
 </html>
--- works.php
+++ works.php
@@ -1,60 +1,63 @@
 <!DOCTYPE HTML>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="favicon.ico">
-        <link href="css/index.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <?php include __DIR__.'/header.php'; ?>
-        <div class="content">
-            <div class="container">
-                <!-- やってきたこと -->
-                <div class="">
-                    <h2>技術的実績</h2>
-                    <h3>言語</h3>
-                    <div class="row mx-auto">
-                        <div class="col-12">
-                            <p>html/pug/css/JavaScript/PHP7.4/Java</p>
-                        </div>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href="css/index.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include __DIR__ . '/header.php'; ?>
+    <div class="content">
+        <div class="container">
+            <!-- やってきたこと -->
+            <div class="">
+                <h2>技術的実績</h2>
+                <h3>言語</h3>
+                <div class="row mx-auto">
+                    <div class="col-12">
+                        <p>html (including pug) / css / JavaScript / PHP7.4 / Java</p>
                     </div>
-                    <h3>フレームワーク(Frontend)</h3>
-                    <div class="row mx-auto">
-                        <div class="col-12">
-                            <p>Vue.js/jQuery/tachyons/Bootstrap3,4/thymeleaf</p>
-                        </div>
-                    </div>
-                    <h3>フレームワーク(Server)</h3>
-                    <div class="row mx-auto">
-                        <div class="col-12">
-                            <p>Laravel/Node.js/Nuxt.js/SpringBoot2/Java EE7</p>
-                        </div>
-                    </div>
-                    <h3>DB・O/Rマッパー</h3>
-                    <div class="row mx-auto">
-                        <div class="col-12">
-                            <p>Oracle11g,12c/MySQL8.0.22/PostgreSQL10/JOOQ</p>
-                        </div>
-                    </div>
-                    <h3>OS・バージョン管理・パッケージ管理・その他</h3>
-                    <div class="row mx-auto">
-                        <div class="col-12">
-                            <p>Mac OS/Windows/Linux(Ubuntu,Debian)/SVN/Git/nvm/npm/yarn</p>
-                        </div>
-                    </div>
-                    <br />
-                    <p>＊その他、詳細な業務経歴などは個別にお問い合わせください。</p>
                 </div>
-                <!-- ページトップへボタン -->
-                <!--<div id="page_top">
+                <h3>フレームワーク(Frontend)</h3>
+                <div class="row mx-auto">
+                    <div class="col-12">
+                        <p>Vue.js / jQuery / tachyons / Bootstrap3,4 / thymeleaf</p>
+                    </div>
+                </div>
+                <h3>フレームワーク(Server)</h3>
+                <div class="row mx-auto">
+                    <div class="col-12">
+                        <p>Laravel / Node.js / Nuxt.js / SpringBoot2 / Java EE7</p>
+                    </div>
+                </div>
+                <h3>DB・O/Rマッパー</h3>
+                <div class="row mx-auto">
+                    <div class="col-12">
+                        <p>Oracle11g,12c / MySQL8.0.22 / PostgreSQL10 / JOOQ</p>
+                    </div>
+                </div>
+                <h3>OSとか</h3>
+                <div class="row mx-auto">
+                    <div class="col-12">
+                        <p>Mac OS / Windows / Linux(Ubuntu,Debian)</p>
+                    </div>
+                </div>
+                <br />
+                <p>＊その他、詳細な業務経歴などは個別にお問い合わせください。</p>
+            </div>
+            <!-- ページトップへボタン -->
+            <!--<div id="page_top">
                     <a href="#"></a>
                 </div> -->
-            </div>
         </div>
-        <?php include __DIR__.'/footer.php'; ?>
-        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
-    </body>
+    </div>
+    <?php include __DIR__ . '/footer.php'; ?>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+</body>
+
 </html>
